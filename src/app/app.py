9a68fe5cf572a5b264fef860be6b59b882f61cf8
xAlessandroC <alexcalvio@hotmail.it>
2024-02-13 15:14:15 +0100

    core - Add intermediate points selection

--- src/app/app.py
+++ src/app/app.py
@@ -21,9 +21,9 @@ START_LONGITUDE = 11.342279
 
 # App state
 class State(Enum):
-    INIT = 1
-    START = 2
-    STOP = 3
+    START = 1
+    STOP = 2
+    INTERMEDIATE = 3
     PATH = 4
 
 app_state = State.START
@@ -34,20 +34,62 @@ map_widget = None
 # Path selection
 starting_point = None
 ending_point = None
+intermediate_points = []
 active_path = None
+
+def submit_task(starting_point, ending_point):
+    starting_position = "{}, {}".format(starting_point.position[0], starting_point.position[1])
+    ending_position = "{}, {}".format(ending_point.position[0], ending_point.position[1])
+    print(starting_position)
+    print(ending_position)
+    graph_name = re.sub(r'[^\w_. -]', '_', f"{str(starting_position).lower().strip()}-{str(ending_position).lower().strip()}")
+    route_dir = route2vel.find_route_osrm([starting_position, ending_position], load_graph=True, load_graph_name=graph_name)
+    inverted_geo = [(x[1], x[0]) for x in route_dir.geometry]
+    print("-------------------")
+    interp_dir = route2vel.interp_from_route(route_dir)
+    print(interp_dir)
+    print("-------------------")
+    route2vel.utils.debug = True
+    sampling_distance = 5
+    sampled_gdf = interp_dir.get_points_with_density(
+        sampling_distance, 
+        return_gdf=True,
+        in_meters=True, 
+        gdf_columns=['base_idx', 'junction', 'speed_kph'] if 'junction' in interp_dir.split_gdf.columns else ['base_idx','speed_kph'] #XXX: fix juncture missing,
+    )
+    print(sampled_gdf)
+    print("-------------------")
+
+    calc_curvature(sampled_gdf)
+
+    csv_path = "route_output_full.csv"
+    csv_path = os.path.abspath(csv_path)
+
+    interp_gdf_to_csv(
+        sampled_gdf, csv_path, 
+        separate_roundabout=True if 'junction' in interp_dir.split_gdf.columns else False,  #XXX: fix juncture missing, 
+        add_tract_start_col=True, 
+        extra_cols=['speed_kph', 'curvature'],
+    )
+
+    return inverted_geo
 
 
 def clearMarkers():
-    global app_state, starting_point, ending_point, active_path
+    global app_state, starting_point, ending_point, active_path, intermediate_points
     print("Current state:", app_state)
 
     map_widget.delete(starting_point)
     map_widget.delete(ending_point)
     map_widget.delete(active_path)
 
+    for point in intermediate_points:
+        map_widget.delete(point)
+
     starting_point = None
     ending_point = None
     active_path = None
+    intermediate_points = []
 
     app_state = State.START
     selected.set('start')
@@ -60,7 +102,7 @@ def checkButton():
    
 
 def left_click_event_on_map(coordinates_tuple):
-    global app_state, starting_point, ending_point
+    global app_state, starting_point, ending_point, intermediate_points
     print("Left click event with coordinates:", coordinates_tuple)
     print("Current state:", app_state)
 
@@ -78,49 +120,25 @@ def left_click_event_on_map(coordinates_tuple):
         # Set new ending marker
         ending_point = map_widget.set_marker(*coordinates_tuple, text="Clickino", marker_color_outside="red", text_color="red", marker_color_circle="red")
 
+    elif app_state == State.INTERMEDIATE:
+        # Set new intermediate marker
+        intermediate_point = map_widget.set_marker(*coordinates_tuple, text="{}".format(len(intermediate_points) + 1), marker_color_outside="orange", text_color="black", marker_color_circle="orange")
+        intermediate_points.append(intermediate_point)
+
 
 def submit():
     global app_state, active_path
     print("Submit clicked")
 
-
     if starting_point is not None and ending_point is not None:
         print("Markers selected")
-        starting_position = "{}, {}".format(starting_point.position[0], starting_point.position[1])
-        ending_position = "{}, {}".format(ending_point.position[0], ending_point.position[1])
-        print(starting_position)
-        print(ending_position)
-        graph_name = re.sub(r'[^\w_. -]', '_', f"{str(starting_position).lower().strip()}-{str(ending_position).lower().strip()}")
-        route_dir = route2vel.find_route_osrm([starting_position, ending_position], load_graph=True, load_graph_name=graph_name)
-        inverted_geo = [(x[1], x[0]) for x in route_dir.geometry]
-        active_path = map_widget.set_path(inverted_geo)
         app_state = State.PATH
-        print("-------------------")
-        interp_dir = route2vel.interp_from_route(route_dir)
-        print(interp_dir)
-        print("-------------------")
-        route2vel.utils.debug = True
-        sampling_distance = 5
-        sampled_gdf = interp_dir.get_points_with_density(
-            sampling_distance, 
-            return_gdf=True,
-            in_meters=True, 
-            gdf_columns=['base_idx', 'junction', 'speed_kph'] if 'junction' in interp_dir.split_gdf.columns else ['base_idx','speed_kph'] #XXX: fix juncture missing,
-        )
-        print(sampled_gdf)
-        print("-------------------")
-    
-        calc_curvature(sampled_gdf)
+        total_points = [starting_point] + intermediate_points + [ending_point]
+        total_inverted_geo = []
+        for i in range(len(total_points) - 1):
+            total_inverted_geo += submit_task(total_points[i], total_points[i+1])
 
-        csv_path = "route_output_full.csv"
-        csv_path = os.path.abspath(csv_path)
-
-        interp_gdf_to_csv(
-            sampled_gdf, csv_path, 
-            separate_roundabout=True if 'junction' in interp_dir.split_gdf.columns else False,  #XXX: fix juncture missing, 
-            add_tract_start_col=True, 
-            extra_cols=['speed_kph', 'curvature'],
-        )
+        active_path = map_widget.set_path(total_inverted_geo)
         
     else:
         print("Markers not selected")
@@ -146,9 +164,11 @@ if __name__ == "__main__":
 
     selected = StringVar()
     r1 = Radiobutton(interaction_frame, text='Start', value='start', variable=selected, command=checkButton)
-    r2 = Radiobutton(interaction_frame, text='Stop', value='stop', variable=selected, command=checkButton)
+    r2 = Radiobutton(interaction_frame, text='Intermediate', value='intermediate', variable=selected, command=checkButton)
+    r3 = Radiobutton(interaction_frame, text='Stop', value='stop', variable=selected, command=checkButton)
     r1.grid(row=0, column=0, padx=10, pady=10)
     r2.grid(row=0, column=1, padx=10, pady=10)
+    r3.grid(row=0, column=2, padx=10, pady=10)
     selected.set('start')
 
     button = Button(interaction_frame, text="Clear", command=clearMarkers)
