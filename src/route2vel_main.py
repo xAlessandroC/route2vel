f69e95760fd5c2fb9fe3a3526b654c77fc197818
xAlessandroC <alexcalvio@hotmail.it>
2024-03-02 17:57:19 +0100

    feat - Add sampling points

--- src/route2vel_main.py
+++ src/route2vel_main.py
@@ -4,6 +4,8 @@ import argparse
 import route2vel
 import socketio
 
+from route2vel.postprocess import calc_curvature
+
 
 parser = argparse.ArgumentParser(description="Route2Vel is a tool to find routes between points and extract velocity profiles")
 parser.add_argument("--start", metavar=("lat", "lon"), type=float, nargs=2, required=True, help="Starting location (lat, lon)")
@@ -53,23 +55,40 @@ if __name__ == "__main__":
             "room": args.websocket_room
         })
         time.sleep(5)
-        ws_client.emit("update", {
-            "message": "Test1",
-            "room": args.websocket_room
-        })
         
         route_dir = route2vel.find_route_osrm([start_location, *intermediate_locations, end_location], load_graph=True, load_graph_name=graph_name)
         print("Route found: {}".format(route_dir))
-        time.sleep(5)
+
         ws_client.emit("update", {
-            "message": "Percorso trovato...",
+            "message": "Interpolazione percorso...",
             "room": args.websocket_room
         })
+        interp_dir = route2vel.interp_from_route(route_dir)
+
+        route2vel.utils.debug = True
+        sampled_gdf = interp_dir.get_points_with_density(
+            args.sampling, 
+            return_gdf=True, 
+            in_meters=False, 
+            gdf_columns=['base_idx', 'junction', 'speed_kph'] if 'junction' in interp_dir.split_gdf.columns else ['base_idx','speed_kph'] #XXX: fix juncture missing,
+        )
+        calc_curvature(sampled_gdf)
+        sampled_points = [[sh_point.x, sh_point.y, sh_point.z] for sh_point in sampled_gdf["geometry"].tolist()]
+        print(sampled_points)
+
+        time.sleep(5)
+        ## Send all the results
         ws_client.emit("route_data", {
             "type": "route",
             "length": route_dir.distance,
             "duration": route_dir.duration,
             "coords": route_dir.geometry,
+            "room": args.websocket_room
+        })
+
+        ws_client.emit("route_data", {
+            "type": "sampling",
+            "coords": sampled_points,
             "room": args.websocket_room
         })
 
